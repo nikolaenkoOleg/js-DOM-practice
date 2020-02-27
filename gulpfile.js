9db90a0a5352383cae2341924457be4e1670264c
Oleg <MNKS@list.ru>
2020-02-27 20:32:43 +1000

    added open server test folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const browsersync = require('browser-sync');
 
-const dist = './dist/';
+// const dist = './dist/';
+const dist = '../../../../OSPanel/domains/test';
 
 gulp.task('copy-html', () => gulp.src('./src/index.html')
   .pipe(gulp.dest(dist))
